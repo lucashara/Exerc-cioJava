e60b0165acda4ebdc198324742ac29b7fb86a708
lucashara <43150035+lucashara@users.noreply.github.com>
2019-04-11 19:27:40 -0300

    q6

---
+++ q6/Auxiliar.java
@@ -0,0 +1,31 @@
+package q6;
+
+public class Auxiliar {
+	float salario;
+	float prestacao;
+
+	public float getSalario() {
+		return salario;
+	}
+
+	public void setSalario(float salario) {
+		this.salario = salario;
+	}
+	
+	public float getPrestacao() {
+		return prestacao;
+	}
+
+	public void setPrestacao(float prestacao) {
+		this.prestacao = prestacao;
+	}
+
+	public void calculaCredito() {
+		if (getPrestacao() < (getSalario() * 0.30)){
+			System.out.println("O crédito será concedido.");
+		}
+		else {
+			System.out.println("Infelizmente sua solicitação não poderá ser atendida.");
+		}
+	}
+}
