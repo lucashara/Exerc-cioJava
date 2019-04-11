86ec4d2879f09e59f2e03c158286cec3f6d6ba65
lucashara <43150035+lucashara@users.noreply.github.com>
2019-04-11 19:08:21 -0300

    q2

---
+++ q2/Principal.java
@@ -0,0 +1,26 @@
+package q2;
+
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+
+		int i = 0;		
+	    Scanner leitor = new Scanner(System.in); 
+
+		for(; i< 10; i++) {
+		    System.out.println("Informe um numero inteiro: ");
+		    int num = leitor.nextInt();
+
+		    if ((num % 2) == 0) {
+	  	    	System.out.println("Par.");
+	  	    }
+	  	    else {
+	  	    	System.out.println("Ímpar.");
+	  	    }
+		}
+		leitor.close();
+	}
+
+}
